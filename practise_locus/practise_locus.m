03a0851d212e81493cea0db439725ec4a0d1e1e2
David Boelke <david.boelke@rwth-aachen.de>
2021-10-19 11:05:14 +0200

    onlie causal systems

--- practise_locus/practise_locus.m
+++ practise_locus/practise_locus.m
@@ -10,29 +10,44 @@ clc
 %- Parameters configure the trainer to your liking with these four settings
 
 % How many roots should the transfer function have at most
-max_many_roots = 5;
+max_many_roots = 2;
 % What is the largest value for a coefficient in the numerator
 max_numerator_coeff = 10;
 % How many poles should the transfer function have at most
-max_many_poles = 5;
+max_many_poles = 2;
 % What is the largest value for a coefficient in the denominator
 max_denom_coeff = 10;
 
 %- Begin of function how many coefficients are we gonna have in the numerator
 % and the denominator?
-many_roots=randi([0,max_many_roots]); many_poles=randi([0,max_many_roots]);
+many_poles=randi([0,max_many_poles]); many_roots=min(randi([0,max_many_roots]), many_poles);
 
 % fill a (the coefficients for the numerator) and b (the coefficients for the
 % denominator)
 a=[];
-for i=1:many_roots
-	a=[a,randi([-max_numerator_coeff,max_numerator_coeff])];
+s=tf('s');
+for it=1:many_roots
+	a=[a,randi([-max_numerator_coeff,max_numerator_coeff])+i*randi([-max_numerator_coeff,max_numerator_coeff])];
+	if imag(a(end))
+		a=[a, conj(a(end))];
+	end
 end
 b=[];
-for j=1:many_poles
-	b=[b,randi([-max_denom_coeff,max_denom_coeff])];
+for jt=1:many_poles
+	b=[b,randi([-max_numerator_coeff,max_numerator_coeff])+i*randi([-max_numerator_coeff,max_numerator_coeff])];
+	if imag(b(end))
+		b=[b, conj(b(end))];
+	end
+end
+g1=1;
+for i =1:length(a)
+	g1=g1*(s-a(i));
+end
+for i =1:length(b)
+	g1=g1/(s-b(i));
 end
 
+%{
 % We can't have empty lists for the numerator or denominator
 if isempty(a)
 	a=1;
@@ -40,11 +55,17 @@ end
 if isempty(b)
 	b=1;
 end
+%}
 
 % The transerfunction is created and introduced
 disp('Please derive the locus for this function:')
 
-g1=tf(a, b)
+g1
+disp('It has these roots:')
+disp(a)
+disp('And these poles:')
+disp(b)
+
 
 % Now the user creates their picture. Once they are finished they press enter
 % and...
@@ -52,5 +73,6 @@ x=input('Are you ready to see the solution?', 's');
 
 % ...is displayed with equally scaled axis
 rlocus(g1);
+disp('Hope you made it.')
 
 axis equal
